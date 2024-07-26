a43fcff2f72391edfbfecf973164aab8129a3f65
Roberto Gonzalez <rob.gonzalez.castro@gmail.com>
2024-07-26 11:07:21 -0400

    agregando javascript al proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 /*Este es un comentario JS*/
 
-alert("Hola Mundo Git!!!")
+alert("Hola Mundo Git 1!!!");
+
+console.log("Hola Mundo Git 2!!!");
+
+alert("Hola Mundo Git 3!!!");
+
+alert("Hola Mundo Git 4!!!");
